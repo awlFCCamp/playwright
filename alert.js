5ef081d20c249aa5f8324adcebacaf7e9909564d
Angela Lee <angelawlee@rocketmail.com>
2023-05-21 14:42:06 -0500

    alert

---
+++ alert.js
@@ -0,0 +1,20 @@
+const { chromium } = require("playwright");
+
+(async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 1000 });
+  const page = await browser.newPage();
+  await page.goto("https://demoqa.com/alerts");
+  page.once("dislog", async (dialog) => {
+    console.log(dialog.message());
+    await dialog.accept();
+  });
+
+  await page.click("#confirmButton");
+
+  page.once("dialog", async (dialog) => {
+    console.log(dialog.message());
+    await dialog.accept("my text is this");
+  });
+  await page.click("#promtButton");
+  await browser.close();
+})();
