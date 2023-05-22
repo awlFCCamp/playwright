3c6eef4a980ca3fe4b017de29d25fcba437ff5ff
Angela Lee <angelalee@Angelas-iMac.local>
2023-05-22 10:28:14 -0500

    mobile modify .gitignore

---
+++ mobile.js
@@ -0,0 +1,14 @@
+const { chromium, devices } = require("playwright");
+const iPhone = devices["iPhone 11"](async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 500 });
+  const context = await browser.newContext({
+    ...iPhone,
+    permissions: ["geolocation"],
+    geolocation: { latitude: 19.432608, longitude: -99.132608 },
+    locale: "en-US",
+  });
+  const page = await context.newPage();
+  await page.goto("https://www.google.com/maps");
+  await page.waitForTimeout(5000);
+  await browser.close();
+})();
